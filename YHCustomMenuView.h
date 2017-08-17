7f749345538fa167a7bbc274b9353a37010c2969
EmyWong <woxiangni321@qq.com>
2017-08-17 17:20:50 +0800

    自定义动画菜单

--- YHCustomMenuView.h
+++ YHCustomMenuView.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface YHCustomMenuView : UIView
-
+//图标
+@property (nonatomic, retain) UIImage *iconImg;
+//整体颜色
+@property (nonatomic, retain) UIColor *themeColor;
+//图片上的icon
+@property (nonatomic, retain) UIImageView *iconImgView;
 @end
--- YHCustomMenuView.m
+++ YHCustomMenuView.m
@@ -7,15 +7,53 @@
 //
 
 #import "YHCustomMenuView.h"
+@interface YHCustomMenuView ()
+//彩色覆盖视图
+@property (nonatomic, retain) UIView *colorfulView;
+//白色底图的彩色线条
+@property (nonatomic, retain) UIView *lineView;
+
+@end
 
 @implementation YHCustomMenuView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addAllSubViews];
+    }
+    return self;
 }
-*/
 
+//添加所有的子视图
+- (void)addAllSubViews {
+    //所有的frame基于父视图设计
+    CGFloat width = self.frame.size.width;
+    CGFloat height = self.frame.size.height;
+    
+    self.lineView =[[UIView alloc] initWithFrame:CGRectMake(width-5,0, 5, height)];\
+    
+    self.iconImgView = [[UIImageView alloc] initWithFrame:CGRectMake(width * 0.35,height *0.35, width *0.3, width *0.3)];
+    self.iconImgView.userInteractionEnabled = YES;
+    
+    self.colorfulView = [[UIView alloc] initWithFrame:CGRectMake(-width, 0,width , height)];
+    
+    [self addSubview:self.lineView];
+    [self addSubview:self.colorfulView];
+    [self addSubview:self.iconImgView];
+    [self bringSubviewToFront:self.iconImgView];
+    
+}
+//设置图标的时候需要使其能够被渲染颜色 方便动画中的变色
+- (void)setIconImg:(UIImage *)iconImg {
+    UIImage *img = [iconImg imageWithRenderingMode:(UIImageRenderingModeAlwaysTemplate)];
+    self.iconImgView.image = img;
+    
+}
+//统一设置主题颜色 使其看起来和谐美观
+- (void)setThemeColor:(UIColor *)themeColor {
+    self.lineView.backgroundColor = themeColor;
+    self.colorfulView.backgroundColor = themeColor;
+    [self.iconImgView setTintColor:themeColor];
+}
 @end
